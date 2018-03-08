0c165f95e92a499334c4027c4316e294cbf91b08
Mr-Chatwin <1071840159@qq.com>
2018-03-09 00:23:38 +0800

    Update config.php

--- web/config.php
+++ web/config.php
@@ -1,9 +1,9 @@
 <?php
 define('ROOT_PART', Root_part());
-define('APIKEY', getenv("APIKEY"));
-define('GJ_CODE', getenv("GJ_CODE"));
-define('SITE_NAME',getenv("SITE_NAME"));
-define('TITLENAME', getenv("TITLENAME"));
-define('EN2DEKEY',getenv("EN2DEKEY"));
-define('EMAIL', getenv("EMAIL"));
+define('APIKEY', 'AIzaSyAv_gLsCHJLYEotdLMc3ncNBR3QVgSx670');
+define('GJ_CODE', 'US');
+define('SITE_NAME', '有图比');
+define('TITLENAME', '有图比');
+define('EN2DEKEY', '5Ubmrn7CJc');
+define('EMAIL', 'cuitong@live.cn');
 ?>
